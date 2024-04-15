c0e64a6bf06805666ad14c9d3c44d9ccffa4a254
ohadbenisti <ohadbenisti@gmail.com>
2024-04-15 15:45:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='bg-amber-500 py-[100px]'>
+     <h1 className="text-5xl font-bold text-amber-100 text-center "> BlaBlaBla </h1>
+     </div>
+
+    </>
+  )
+}
+
+export default App
